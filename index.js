957d97b23918bdb2427a639d66a8a4a6536c19d7
abby krause <abbykrause@MacBook-Pro-3.local>
2019-11-21 17:45:59 -0600

    added restaurant api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let flyFrom = null;
 let flyTo = null;
 let fromDate = null;
 let toDate = null;
+let city = null;
+let cityID = null;
 let limit = 5;
 
 function nextPage(){
@@ -19,42 +21,76 @@ function getParams(){
        flyTo = $('.dest').val();
        fromDate = $('.from').val();
        toDate = $('.to').val();
+       city = $('.city').val();
        $('.parameterPage').hide();
-       $('.resultsPage').show();
        generateFlights();
-        
+       generateCityId();
+       
+          
     })
 
 }
 
-function generateFlights(){
-    let url = generateURL();
-    fetch(`${url}`)
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
+function generateCityId(){
     
+    console.log(city)
+    const options = {
+
+        headers: new Headers({
+            "user-key": "4d1fd27573b6d91183797cc18f880b5e"
+        })
+    };
+
+    fetch(`https://developers.zomato.com/api/v2.1/locations?query=${city}%20`, options)
+    .then(response => response.json())
+    .then(responseJson => generateRestaurants(responseJson.location_suggestions[0].city_id))
+    
+    
+       
 }
 
-function test(){
-    $('.test').on('click',function(e){
-        event.preventDefault();
-        generateFlights();
-    })
-}
-
-function displayResults(responseJson){
-    for(let i = 0; i < responseJson.data.length; i ++){
-        $('.test').hide();
-        $('.resultsPage').append(`<p>${responseJson.data[i].flyFrom}=>${responseJson.data[i].flyTo}<a target="blank" href=${responseJson.data[i].deep_link}> here! </a></p>`)
-    }
-}
-
-function generateURL(){
-    let url = `https://api.skypicker.com/flights?fly_from=city:${flyFrom}&fly_to=city:${flyTo}&partner=picky&currency=USD&date_from=${fromDate}&date_to=${toDate}&conversion=USD&partner_market=US&limit=5`
+function generateFlightURL(){
+    let url = `https://api.skypicker.com/flights?fly_from=city:${flyFrom}&fly_to=city:${flyTo}&partner=picky&curr=USD&currency=USD&date_from=${fromDate}&date_to=${toDate}&conversion=USD&partner_market=US&limit=5`
     return url
 }
 
+function generateFlights(){
+    let url = generateFlightURL();
+    fetch(`${url}`)
+    .then(response => response.json())
+    .then(responseJson => displayFlightResults(responseJson)) 
+}
 
+function displayFlightResults(responseJson){
+    for(let i = 0; i < responseJson.data.length; i ++){
+        $('.test').hide();
+        $('.resultsPage').show();
+        $('.resultsPage').find('.flightResults').append(`<a target="blank" href=${responseJson.data[i].deep_link}> ${responseJson.data[i].flyFrom}=>${responseJson.data[i].flyTo} </a>`)
+    }
+}
+
+
+function generateRestaurants(id){
+    
+    const options = {
+        headers: new Headers({
+            "user-key": "4d1fd27573b6d91183797cc18f880b5e"
+        })
+    };
+
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${id}&entity_type=city&q=food&count=5`,options)
+    .then(response => response.json())
+    .then(responseJson => displayRestaurantResults(responseJson))
+}
+
+
+
+function displayRestaurantResults(responseJson){
+    for(let i = 0; i < 5; i ++){
+        $('.resultsPage').find('.restaurantResults').append(`<a>${responseJson.restaurants[i].restaurant.name}</a>`)
+        console.log(responseJson.restaurants[i].restaurant.name)
+    }
+}
 
 
 
@@ -66,7 +102,7 @@ function generateURL(){
 function domReady(){
     $(nextPage);
     $(getParams);
-    $(test);
+    
 
 }
 
